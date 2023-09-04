147b8b6845a76477d9ea59804747fae342edf6c6
Keenan Deyce <keenandeyce@gmail.com>
2023-09-04 21:12:53 +0200

    Update sendEmail.js
    - Used onAdd instead of onSnapshot to access added document.

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -16,37 +16,35 @@ let docRef = db.collection("messages");
 let sentEmails = new Set();
 
 function sendEmail() {
-    docRef.orderBy("timestamp", "desc").limit(1).onSnapshot(querySnapshot => {
-        querySnapshot.forEach(doc => {
-            let docId = doc.id;
-            if (sentEmails.has(docId)) {
-                return; // Skip this document, we've already processed it.
-            }
-            sentEmails.add(docId);
+    docRef.onAdd((doc) => {
+        let docId = doc.id;
+        // Check if the document has already been processed
+        if (sentEmails.has(docId)) {
+            return;
+        }
+        sentEmails.add(docId);
 
-            let data = doc.data();
+        let data = doc.data();
 
-            const msg = {
-                to: 'keenandeyce@gmail.com',
-                from: data['email'],
-                subject: data['subject'],
-                text: `Message from ${data['fullName']} (${data['email']}): ${data['message']}`,
-                html: `<strong>Message from ${data['fullName']} (${data['email']}):</strong> ${data['message']}`,
-            };
+        const msg = {
+            to: 'keenandeyce@gmail.com',
+            from: data['email'],
+            subject: data['subject'],
+            text: `Message from ${data['fullName']} (${data['email']}): ${data['message']}`,
+            html: `<strong>Message from ${data['fullName']} (${data['email']}):</strong> ${data['message']}`,
+        };
 
-            sgMail
-                .send(msg)
-                .then((response) => {
-                    console.log('Email sent!!')
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.error(error)
-                })
-        });
-    }, err => {
-        console.log(`Encountered error: ${err}`);
+        sgMail
+            .send(msg)
+            .then((response) => {
+                console.log('Email sent!!')
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
     });
-}
+};
+
 
 module.exports = sendEmail
