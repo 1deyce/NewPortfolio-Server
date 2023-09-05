49f2115a5f7128f2a913a85fd0ef94addd88259c
Keenan Deyce <keenandeyce@gmail.com>
2023-09-05 13:21:58 +0200

    Fix
    - Passed params in server post
    - Using bodyParser to parse json object.

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const app = express();
 const port = process.env.PORT
 const cors = require('cors');
 const sendEmail = require("./src/sendEmail.js");
+const bodyParser = require("body-parser");
 
 app.use(cors({
     origin: '*',
     credentials: true,
 }));
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send("My Homepage");
 })
 
 app.post('/email', (req, res) => {
-    
-    sendEmail();
+    const { fullName, email, subject, message } = req.body;
+    sendEmail({ fullName, email, subject, message });
     res.send("Backend POST success");
 })
 
