1867756696bf4f57e29d166f8b9c08c192dfddd9
Keenan Deyce <keenandeyce@gmail.com>
2023-09-05 13:26:42 +0200

    Update sendEmail.js

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -7,44 +7,42 @@ sgMail.setApiKey(process.env.EMAIL_API, {
 });
 
 function sendEmail(data) {
-    // fullName,
-    // email,
-    // subject,
-    // message,
     const msg = {
         to: 'keenandeyce@gmail.com',
-        from: data[email],
-        subject: data[subject],
+        from: data.email,
+        subject: data.subject,
         text: `Hello,
-    
-        You have received a message from ${data[fullName]} (${data[email]}).
-    
+
+        You have received a message from ${data.fullName} (${data.email}).
+
         Message:
-        ${data[message]}
-    
+        ${data.message}
+
         Regards,
         Your Name`,
         html: `<p>Hello,</p>
-    
-        <p>You have received a message from <strong>${data[fullName]}</strong> (${data[email]}).</p>
-    
+
+        <p>You have received a message from <strong>${data.fullName}</strong> (${data.email}).</p>
+
         <p>Message:<br>
-        ${data[message]}</p>
-    
+        ${data.message}</p>
+
         <p>Regards,<br>
         Your Name</p>`,
     };
 
-    try {
-        const response = sgMail.send(msg);
-        console.log("Email sent!")
-        console.log(response);
-    } catch (error) {
-        console.error(error);
-    };
-
+    return new Promise((resolve, reject) => {
+        sgMail.send(msg)
+            .then((response) => {
+                console.log("Email sent!");
+                console.log(response);
+                resolve(response);
+            })
+            .catch((error) => {
+                console.error(error);
+                reject(error);
+            });
+    });
 }
-
-
 
 module.exports = sendEmail;
